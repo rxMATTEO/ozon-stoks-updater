04ed2d6e9bead7bdb4d8241039180f2b1cfdd620
rxMATTEO <erik_sundberg@mail.ru>
2024-02-17 17:45:04 +0300

    Add updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import {computed, inject, ref} from "vue";
 import axios from "axios";
-import { Socket, io } from "socket.io-client";
+import {Socket, io} from "socket.io-client";
 import {useToast} from "primevue/usetoast";
+import {FilterMatchMode} from "primevue/api";
+
 const toast = useToast();
 
 const apiUrl = inject('apiUrl');
@@ -14,10 +16,11 @@ async function getOzonList() {
 }
 
 async function updateOzonList() {
-  return  (await axios.post(`${apiUrl}/update/ltm`, {
+  return (await axios.post(`${apiUrl}/update/ltm`, {
     ozonList: ozonItems.value
   })).data;
 }
+
 const socket: Socket = io('http://localhost:3001');
 socket.emit('connection');
 socket.on('ozonUpdate', ({result}) => {
@@ -41,23 +44,42 @@ const columns = computed(() => {
 const rowsPerPage = computed(() => {
   return [10, 20, 50];
 });
+
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+});
 </script>
 
 <template>
+  <Toast />
   <div class="p-2" v-cloak>
     <div class="flex justify-content-between align-items-center">
       <Button icon="pi pi-cloud-download" label="Получить список товаров" @click="getOzonList"></Button>
-      <Button :disabled="!ozonItems.length" icon="pi pi-cloud-upload" label="Обновить остатки" @click="updateOzonList"></Button>
+      <Button :disabled="!ozonItems.length" icon="pi pi-cloud-upload" label="Обновить остатки"
+              @click="updateOzonList"></Button>
     </div>
     <div class="mt-3">
-      <DataTable :value="ozonItems" selectionMode="single" paginator
-                 :rows="10" :rowsPerPageOptions="rowsPerPage"
+      <DataTable v-model:filters="filters" :value="ozonItems" selectionMode="single" paginator
+                 :rows="10" :rowsPerPageOptions="rowsPerPage" :global-filter-fields="['offer_id']"
                  removableSort showGridlines filterDisplay="row">
+        <template #header>
+          <div class="flex justify-content-end">
+              <InputText v-model="filters['global'].value" placeholder="Поиск по артикулу" />
+          </div>
+        </template>
         <Column v-for="[k,{header, field}] in Object.entries(columns)" :header="header" :field="field" sortable
-                :key="k" />
+                :key="k">
+          <template #body="{data}">
+            <div :class="{'text-green-500': updatedOzonItems.some(i => i.product_id === data.product_id)}">
+              {{ data[field] }}
+            </div>
+          </template>
+        </Column>
         <Column header="Обновлено">
-          <template #body>
-            Да
+          <template #body={data}>
+            <div :class="{'text-green-500': updatedOzonItems.some(i => i.product_id === data.product_id)}">
+              {{ updatedOzonItems.some(i => i.product_id === data.product_id) ? 'Да' : 'Нет' }}
+            </div>
           </template>
         </Column>
       </DataTable>
